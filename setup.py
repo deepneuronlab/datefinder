4469fb12a6dbe5f8c1437ab9e3a03f3fc0aa0efd
IvanSpirandelli <ivan@spirandelli.de>
2021-02-22 10:25:47 +0100

    adjusted setup.py to reflect ownership

--- setup.py
+++ setup.py
@@ -6,10 +6,15 @@ https://github.com/pypa/sampleproject
 """
 import setuptools
 setuptools.setup(
-    name="datefinder-dnl-extension",  # Replace with your own username
+    name="datefinder-dnl-extension",
     version="0.0.1",
-    author="DNL",
-    author_email="info@dnl.com",
+
+    # The project's main homepage. This particular fork extends the main project in terms of the needs of Berlin based
+    # company DNL. To reach DNL write a message to info@dnl.de. In this setup file we reference the original author.
+    url="https://github.com/akoumjian/datefinder",
+    author="Alec Koumjian",
+    author_email="akoumjian@gmail.com",
+
     description="Extension of the datefinderpackage for DNL",
     packages=setuptools.find_packages(),
     classifiers=[
